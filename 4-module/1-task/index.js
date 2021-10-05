fa1c783f3fc6d50bcad6d2bc5931061c6e73c624
evs <evstigney2010@gmail.com>
2021-10-05 19:51:09 +0300

    Решение задачи про список друзей

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,15 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList (friends) {
+  let friendsList = document.createElement('ul');
+
+	for (let friend of friends) {
+		friendsList.append(makeFriendItem(friend));
+	}
+
+	return friendsList;
+}
+
+function makeFriendItem (friend) {
+	let friendItem = document.createElement('li');
+	friendItem.textContent = `${friend.firstName} ${friend.lastName}`;
+	return friendItem;
 }
