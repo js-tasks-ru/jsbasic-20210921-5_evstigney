9dc64d728743959fbaac8fa016673692adccabfe
evs <evstigney2010@gmail.com>
2021-10-13 10:06:19 +0300

    Решение задачи про карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,55 @@
+'use strict';
 function initCarousel() {
-  // ваш код...
+	const carousel = document.querySelector('.carousel');
+	const slider = carousel.querySelector('.carousel__inner');
+	const slides = slider.querySelectorAll('.carousel__slide');
+	const width = slider.getBoundingClientRect().width;
+	const arrows = {
+		left: carousel.querySelector('.carousel__arrow_left'),
+		right: carousel.querySelector('.carousel__arrow_right'),
+	};
+	const shift = {
+		max: 0,
+		min: -slides.length * width + width,
+		current: 0,
+	};
+
+	checkArrows(arrows);
+	carousel.addEventListener('click', (event) => {
+		if (event.target.closest('.carousel__arrow')) slideCarousel(event);
+	});
+
+
+	function slideCarousel (event) {
+		const target = event.target.closest('.carousel__arrow');
+		const slide = {
+			right: () => {
+				shift.current -= width;
+
+				if (shift.current <= shift.min) shift.current = shift.min;
+
+				checkArrows(arrows);
+				return shift.current;
+			},
+			left: () => {
+				shift.current += width;
+
+				if (shift.current >= shift.max) shift.current = shift.max;
+
+				checkArrows(arrows);
+				return shift.current;
+			},
+		}
+		let translateX = 0;
+
+		if (target === arrows.right) translateX = slide.right();
+		if (target === arrows.left) translateX = slide.left();
+
+		slider.style.transform = `translateX(${translateX}px)`;
+	}
+
+	function checkArrows (arrows) {
+		arrows.right.style.display = (shift.current <= shift.min) ? 'none' : 'flex';
+		arrows.left.style.display = (shift.current >= shift.max) ? 'none' : 'flex';
+	}
 }
