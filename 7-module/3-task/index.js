3c3cd0d277bc2b72854f5c76fe921d2f50438cc1
evs <evstigney2010@gmail.com>
2021-10-26 16:16:33 +0300

    отрисовала слайдер

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,37 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+		this._steps = steps;
+		this._value = value;
+		this._slider = this._render();
   }
+
+	get elem () {
+		return this._slider;
+	}
+
+	_render () {
+		const slider = this._createSliderElement();
+		const activeStepClass = 'slider__step-active';
+		const steps = slider.querySelectorAll('.slider__steps span');
+		steps[0].classList.add(activeStepClass);
+		return slider;
+	}
+
+	_createSliderElement () {
+		const markup = `
+			<div class="slider">
+				<div class="slider__thumb" style="left: 50%;">
+					<span class="slider__value">2</span>
+				</div>
+				<div class="slider__progress" style="width: 50%;"></div>
+				<div class="slider__steps">
+					${new Array(this._steps).fill('<span></span>').join('')}
+				</div>
+			</div>
+		`.trim();
+		let sliderElement = document.createElement('div');
+		sliderElement.innerHTML = markup;
+		sliderElement = sliderElement.firstChild;
+		return sliderElement;
+	}
 }
