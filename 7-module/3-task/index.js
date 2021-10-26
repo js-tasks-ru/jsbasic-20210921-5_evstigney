b25db689c73c858c71e0d433480ca16b1a881691
evs <evstigney2010@gmail.com>
2021-10-26 17:51:50 +0300

    Решение задачи "Пошаговый слайдер, часть 1"

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -11,9 +11,7 @@ export default class StepSlider {
 
 	_render () {
 		const slider = this._createSliderElement();
-		const activeStepClass = 'slider__step-active';
-		const steps = slider.querySelectorAll('.slider__steps span');
-		steps[0].classList.add(activeStepClass);
+		slider.addEventListener('click', this._sliderClickHandler.bind(this));
 		return slider;
 	}
 
@@ -32,6 +30,33 @@ export default class StepSlider {
 		let sliderElement = document.createElement('div');
 		sliderElement.innerHTML = markup;
 		sliderElement = sliderElement.firstChild;
+		sliderElement.querySelectorAll('.slider__steps span')[0].classList.add('slider__step-active');
 		return sliderElement;
+	}
+
+	_sliderClickHandler (event) {
+		const slider = event.target.closest('.slider');
+		const steps = slider.querySelectorAll('.slider__steps span');
+		const stepWidth = slider.clientWidth / (this._steps - 1);
+		const clickPosition = event.clientX - slider.offsetLeft - slider.clientLeft;
+		const newStep = Math.round(clickPosition / stepWidth);
+
+		if (this._value === newStep) return;
+
+		this._value = newStep;
+		slider.querySelector('.slider__value').textContent = newStep;
+		[...steps].find((step) => step.classList.contains('slider__step-active'))
+			.classList.remove('slider__step-active');
+		steps[newStep].classList.add('slider__step-active');
+		
+		const stepWidthInPercents = Math.round(stepWidth * 100 / slider.clientWidth);
+		slider.querySelector('.slider__thumb').style.left = `${newStep * stepWidthInPercents}%`;
+		slider.querySelector('.slider__progress').style.width = `${newStep * stepWidthInPercents}%`;
+
+		this._slider.dispatchEvent(new CustomEvent('slider-change', {
+			detail: this._value,
+			bubbles: true,
+		}));
+
 	}
 }
