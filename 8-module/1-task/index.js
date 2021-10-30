39409164a6eaeae784398cecfdf6300ab47c940c
evs <evstigney2010@gmail.com>
2021-10-30 16:29:07 +0300

    выполнила первое задание 8го модуля - предварительно

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -8,13 +8,26 @@ export default class CartIcon {
   }
 
   render() {
-    this.elem = createElement('<div class="cart-icon"></div>');
+		// МОЙ ВАРИАНТ УДАЛИТЬ!!!!!!!!
+		const markup = `
+		<div class="cart-icon">
+		<div class="cart-icon__inner">
+          <span class="cart-icon__count"></span>
+          <span class="cart-icon__price"></span>
+        </div>
+		</div>
+		`.trim();
+		this.elem = createElement(markup);
+
+
+
+		// ИСХОДНЫЙ ВАРИАНТ ВЕРНУТЬ!!!!!!!!
+    // this.elem = createElement('<div class="cart-icon"></div>');
   }
 
   update(cart) {
     if (!cart.isEmpty()) {
       this.elem.classList.add('cart-icon_visible');
-
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
@@ -39,6 +52,48 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    
+		// if (!this.elem.offsetWidth) return;
+
+		const WINDOW_MOBILE_WIDTH = 767;
+
+		// if (document.body.clientWidth <= WINDOW_MOBILE_WIDTH) {
+		// 	this._resetStyles();
+		// 	return;
+		// };
+
+		if (!document.body.scrollTop) this._resetStyles();
+		
+		const ICON_MARGINS = {
+			top: 50,
+			right: 10,
+			left: 20,
+		};
+    this.elem.style.display = 'block';
+		const leftIndent = Math.min(
+			document.querySelector('.container').getBoundingClientRect().right + ICON_MARGINS.left,
+  		document.documentElement.clientWidth - this.elem.offsetWidth - ICON_MARGINS.right
+		);
+		Object.assign(this.elem.style, {
+			position: 'fixed',
+			top: `${ICON_MARGINS.top}px`,
+			zIndex: 1e3,
+			right: `${ICON_MARGINS.right}px`,
+			left: `${leftIndent}px`,
+		});
+
+		// this._fixIcon(ICON_MARGINS);
   }
+
+	// _fixIcon (margins) {
+		
+	// }
+
+	_resetStyles () {
+		Object.assign(this.elem.style, {
+			position: '',
+			top: '',
+			left: '',
+			zIndex: ''
+		});
+	}
 }
